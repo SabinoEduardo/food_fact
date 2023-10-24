14dcd40740155690ddb2de3ab885764ce5daf5be
Eduardo <bilson@outlook.com.br>
2023-10-24 11:35:13 -0300

    Trabalhei na validação dos produtos

--- food_fact/templatetags/serializer_tag.py
+++ food_fact/templatetags/serializer_tag.py
@@ -4,11 +4,12 @@ import json
 register = template.Library()
 
 @register.simple_tag
-def json_dumps(data):
+def json_products(data):
     dict_product = dict()
     lista_products = list()
+
     for value in data:
-        dict_product['code'] = int(value.code)
+        dict_product['code'] = value.code
         dict_product['barcode'] = value.barcode
         dict_product['status'] = value.status
         dict_product['imported_t'] = str(value.imported_t.strftime("%d/%m/%Y %H:%M:%S"))
@@ -22,3 +23,22 @@ def json_dumps(data):
         lista_products.append(dict_product.copy())
     json_products = json.dumps(lista_products, indent=4, ensure_ascii=False)
     return json_products
+
+@register.simple_tag
+def json_one_product(data):
+    dict_product = dict()
+    dict_product['code'] = data.code
+    dict_product['barcode'] = data.barcode
+    dict_product['status'] = data.status
+    dict_product['imported_t'] = str(data.imported_t.strftime("%d/%m/%Y %H:%M:%S"))
+    dict_product['url'] = data.url
+    dict_product['product_name'] = data.product_name
+    dict_product['quantity'] = data.quantity
+    dict_product['categories'] = data.categories
+    dict_product['packaging'] = data.packaging
+    dict_product['brands'] = data.brands
+    dict_product['image_url'] = data.image_url
+    lista_products = [(dict_product.copy())]
+    json_products = json.dumps(lista_products, indent=4, ensure_ascii=False)
+    return json_products
+
--- food_fact/urls.py
+++ food_fact/urls.py
@@ -4,5 +4,9 @@ from food_fact import views
 app_name = 'food_fact'
 
 urlpatterns = [
-    path('', views.list_products, name='base'),
+    #Listar os produtos
+    path('products', views.list_products, name='list'),
+
+    #Buscar um produto
+    path('products/<int:code_product>/', views.get_product, name='product')
 ]
