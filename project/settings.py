5e47e4219ce60a1526cc6e3970e93b27272a3bad
Sabino Afonso <afonsos@yahoo.com>
2023-11-13 12:21:58 -0300

    adicionado o arquivo static

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-c08a*gj=i&v4pb+(n8i%9mx=h^*1dhbp4z50phlsq4)()4_-^@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -106,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'pt-BR'
 
-TIME_ZONE = "America/Sao_paulo"
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -117,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'static/'
 
 
 # Default primary key field type
